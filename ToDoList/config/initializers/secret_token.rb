5ef299fb4f4187176bd11f3b084642078a3eb561
russellvaughan <russellvaughan@gmail.com>
2016-04-23 07:34:17 +0100

    Install Rails API Backend Install -add 'rspec-rails', 'capybara', 'database-cleaner, 'selenium-webdriver' to gemfile -Rails generate rspec:install -Add Format Documentation to .rpsec helper -Configure Database cleaner in spec_helper

---
+++ ToDoList/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ToDoList::Application.config.secret_token = '653aa6a0bdbb5001ffa4139f48652518c89c984be06ad627819d3944e51e672cc19202841ad850f632d4e887b40ef9e47852d416401a4b3ae48a4eb780f89934'
